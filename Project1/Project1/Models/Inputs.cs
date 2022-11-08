f9b2bbb9c5e65c07fa273a6a98470a7635d580ab
ZeynepHande <zeynephandega@gmail.com>
2022-11-08 21:58:27 +0300

    Project1

---
+++ Project1/Project1/Models/Inputs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Models
+{
+    public class Inputs
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Inputs()
+        {
+        }
+
+        public Inputs(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,71 @@
+﻿using Project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = GetSampleData();
+            CalculateResult(data);
+        }
+
+        private static List<Inputs> GetSampleData()
+        {
+            var samples = new List<Inputs>();
+
+            samples.Add(new Inputs(1, 2));
+            samples.Add(new Inputs(3, 7));
+            samples.Add(new Inputs(0, 6));
+            samples.Add(new Inputs(1, 1));
+            samples.Add(new Inputs(-5, 2));
+            samples.Add(new Inputs(1, 5));
+            samples.Add(new Inputs(3, -5));
+
+            return samples;
+        }
+
+        private static void CalculateResult(List<Inputs> data)
+        {
+            var parallelX = new List<int>();
+            var parallelY = new List<int>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = i; j < data.Count; j++)
+                {
+                    if (data[i].X == data[j].X && data[i].Y != data[j].Y)
+                    {
+                        if (!parallelY.Contains(data[i].X))
+                        {
+                            parallelY.Add(data[i].X);
+                        }
+                    }
+                    else if (data[i].Y == data[j].Y && data[i].X != data[j].X)
+                    {
+                        if (!parallelX.Contains(data[i].Y))
+                        {
+                            parallelX.Add(data[i].Y);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("X Eksenine Paralel Doğruların Sayısı: " + parallelX.Count());
+            foreach (var item in parallelX)
+            {
+                Console.WriteLine($"\ty={item}");
+            }
+
+            Console.WriteLine("Y Eksenine Paralel Doğruların Sayısı: " + parallelY.Count());
+            foreach (var item in parallelY)
+            {
+                Console.WriteLine($"\tx={item}");
+            }
+
+        }
+    }
+}
